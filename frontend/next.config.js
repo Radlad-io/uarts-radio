0b4e9653dba8fce2f4955db66f811ebab1e51860
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-10-06 18:49:50 -0400

    Changed TTL for deploy

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -6,6 +6,6 @@ module.exports = {
       deviceSizes: [640,750,828,1080,1200,2048,3840],
       imageSizes: [16,32,48,64,96,128,256,384],
       domains: ['uarts-radio.kevinmerinsky.com'],
-      minimumCacheTTL: 60*30,
+      minimumCacheTTL: 600,
     },
   }
