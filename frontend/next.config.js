9001c6fa70fa87bc6cadbda77511a7e359997737
Kevin Merinsky <kevinmerinsky@gmail.com>
2021-10-06 20:17:55 -0400

    Added Dev Branch

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
 // next.config.js
-
 module.exports = {
     images: {
-      // Content API 
       deviceSizes: [640,750,828,1080,1200,2048,3840],
       imageSizes: [16,32,48,64,96,128,256,384],
       domains: ['uarts-radio.kevinmerinsky.com'],
